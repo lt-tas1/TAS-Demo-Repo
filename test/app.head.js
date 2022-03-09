321c8974e64d0d2c93ff0e456a09786804b1ab47
lt-tas1 <99581868+lt-tas1@users.noreply.github.com>
2022-03-09 12:11:25 +0530

    Update app.head.js

--- test/app.head.js
+++ test/app.head.js
@@ -16,6 +16,19 @@ describe('HEAD', function(){
     .head('/tobi')
     .expect(200, done);
   })
+  
+  it('new should default to GET', function(done){
+    var app = express();
+
+    app.get('/tobi', function(req, res){
+      // send() detects HEAD
+      res.send('tobi');
+    });
+
+    request(app)
+    .head('/tobi')
+    .expect(200, done);
+  })
 
   it('should output the same headers as GET requests', function(done){
     var app = express();
