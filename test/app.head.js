acdeb6a30f738ff9c4ff86553453ce591db78310
lt-tas1 <99581868+lt-tas1@users.noreply.github.com>
2022-03-09 12:19:46 +0530

    delete test

--- test/app.head.js
+++ test/app.head.js
@@ -17,19 +17,6 @@ describe('HEAD', function(){
     .expect(200, done);
   })
   
-  it('new should default to GET', function(done){
-    var app = express();
-
-    app.get('/tobi', function(req, res){
-      // send() detects HEAD
-      res.send('tobi');
-    });
-
-    request(app)
-    .head('/tobi')
-    .expect(200, done);
-  })
-
   it('should output the same headers as GET requests', function(done){
     var app = express();
 
