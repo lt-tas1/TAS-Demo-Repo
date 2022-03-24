0e42ae4b20efa4fa6a623478d30e67f0f9ab2816
lt-tas1 <99581868+lt-tas1@users.noreply.github.com>
2022-03-24 14:40:27 +0530

    Update Router.js

--- test/Router.js
+++ test/Router.js
@@ -14,15 +14,6 @@ describe('Router', function(){
     assert(typeof router.handle === 'function')
     assert(typeof router.use === 'function')
   });
-  
-  it('new should return a function with router methods', function() {
-    var router = new Router();
-    assert(typeof router === 'function')
-
-    assert(typeof router.get === 'function')
-    assert(typeof router.handle === 'function')
-    assert(typeof router.use === 'function')
-  });
 
   it('should support .use of other routers', function(done){
     var router = new Router();
